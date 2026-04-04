1001fc4560ee4081889a4afce7c988a66b87a359
GilMelnik <gil@melnik.co.il>
2026-04-05 01:04:54 +0300

    Upgrade version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.colorbounce.baby"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "2.3"
+        versionCode = 8
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
