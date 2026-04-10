04becf04c6481f0af2c2a182163258a838c29945
GilMelnik <gil@melnik.co.il>
2026-04-10 20:09:00 +0300

    small fix

--- app/src/main/java/com/colorbounce/baby/TutorialScreen.kt
+++ app/src/main/java/com/colorbounce/baby/TutorialScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
@@ -70,7 +71,7 @@ private const val FORMATION_ADVANCE_DELAY_MS = 4500L
 
 @Composable
 fun TutorialScreen(onDismiss: () -> Unit) {
-    var currentStep by rememberSaveable { mutableStateOf(0) }
+    var currentStep by rememberSaveable { mutableIntStateOf(0) }
 
     fun nextStep() {
         if (currentStep < STEP_COUNT - 1) {
