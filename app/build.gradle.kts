992ca29757806e4d54f194d5408b0d27eae7a239
GilMelnik <gil@melnik.co.il>
2026-05-04 23:42:44 +0300

    update versionCode to 11 and versionName to 3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.colorbounce.baby"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "3.0"
+        versionCode = 11
+        versionName = "3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
